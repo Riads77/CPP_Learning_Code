030d1e2905e0e0d0a274d4301db7b4fe23298d84
Riads77 <adrienroumegous@hotmail.com>
2022-01-10 22:13:43 +0100

    arg parse

--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
-int main() {
-    int array[50] = {};
+int main(int argc, char** argv) {
+    if (argc < 2)
+    {
+        std::cerr << "Must have at least one argument." << std::endl;
+        return -1;
+    }
 
-    for (int i = 0; i < 50; ++i)
-    { array[i] = i + 1; }
+    std::cout << argv[1] << std::endl;
 
-    for (int value : array)
-    { std::cout << value << std::endl; }
+    // int array[50] = {};
+
+    // for (int i = 0; i < 50; ++i)
+    // { array[i] = i + 1; }
+
+    // for (int value : array)
+    // { std::cout << value << std::endl; }
 
     return 0;
 }
