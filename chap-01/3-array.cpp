406bf60099f3e5c9a7cadf435cf05f3dd1f7e74c
Riads77 <adrienroumegous@hotmail.com>
2022-01-10 20:26:22 +0100

    array 50

--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 int main() {
-    int array[] = { 0, 1, 2, 3 };
+    int array[50] = {};
+
+    for (int i = 0; i < 50; ++i)
+    { array[i] = i + 1; }
 
     for (int value : array)
     { std::cout << value << std::endl; }
