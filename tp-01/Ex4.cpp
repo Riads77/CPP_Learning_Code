8fa2292642632d08f2956c53314b4207285810e1
Riads77 <adrienroumegous@hotmail.com>
2022-01-17 19:53:51 +0100

    Exercice 1

---
+++ tp-01/Ex4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+std::vector<unsigned int> count_lower(const std::string& s)
+{
+    using namespace std;
+
+    vector<unsigned int> counts();
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        char c = s[i];
+        if (islower(c))
+        {}
+    }
+}
+
+void display_lower_occ(const std::vector<unsigned int>& counts)
+{}
+
+int main()
+{
+    using namespace std;
+    string s;
+    cin >> s;
+}
