edc7268ee23028b4202cb0655f8926e05c200a63
Riads77 <adrienroumegous@hotmail.com>
2022-01-24 20:16:52 +0100

    chap 3 / durée de vie / fin

--- chap-03/1-lifespan/Box.cpp
+++ chap-03/1-lifespan/Box.cpp
@@ -20,15 +20,15 @@ int main()
 
     std::cout << "Main start" << std::endl;
 
-    Box*    box     = new Box("gift");
-    Content content = box->get_content();
+    Box*     box         = new Box("gift");
+    Content& content_ref = box->get_content();
 
-    std::cout << content.name << std::endl;
+    std::cout << content_ref.name << std::endl;
 
     delete box;
 
-    content.name = "chocolate";
-    std::cout << content.name << std::endl;
+    content_ref.name = "chocolate";
+    std::cout << content_ref.name << std::endl;
 
     std::cout << "Main end" << std::endl;
 
